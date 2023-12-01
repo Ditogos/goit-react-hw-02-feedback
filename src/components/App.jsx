09fbd7f4740521d78487c9a7a68a121264dc336a
Artem <krylov.artem.1995@gmail.com>
2023-12-01 13:32:12 +0100

    update first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+
+export class FeedbackOption extends Component {
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+render(){
+  <Wrapper>
+    <h1 className="State"></h1>
+    <ul className="ListFeedback">
+      <li>Good</li>
+      <li>Neutral</li>
+      <li>Bad</li>
+    </ul>
+  </Wrapper>
+}
+}
